4ef748d67939dd107f1e6000e8804a84bee52992
Dave Neeley <dneeley@spillman.com>
2017-07-26 12:58:50 -0600

    add date filter

--- report.ps1
+++ report.ps1
@@ -1,7 +1,9 @@
 param (
 $credential,
 [int]$generations = 8,
-$placeFilter = "\bIL\b|\bIA\b|iowa|illin"
+$placeFilter = "\bIL\b|\bIA\b|iowa|illin",
+[DateTime]$dateStart = '1830-01-01',
+[DateTime]$dateEnd = '1850-12-31'
 )
 $file = '2017-07-26.txt'
 if (!(test-path $file)) {
@@ -29,7 +31,7 @@ function sanitizeDate {
 
 foreach ($placeprop in @('birtplac', 'chrplac', 'deatplac', 'buriplace')) {
     $dateprop = $placeprop -replace "plac", "date"
-    $allData | ?{$_.$placeprop -match $placeFilter} | %{new-object PSobject -property @{placeprop=$placeprop;name=$($_.given + " " + $_.surname);place=$_.$placeprop;date=sanitizeDate($_.$dateprop);fid=$_.fid}} | select placeprop, date, name, place, fid | sort date | Format-Table -autosize
+    $allData | ?{$_.$placeprop -match $placeFilter} | %{new-object PSobject -property @{placeprop=$placeprop;name=$($_.given + " " + $_.surname);place=$_.$placeprop;date=sanitizeDate($_.$dateprop);fid=$_.fid}} | select placeprop, date, name, place, fid | ?{!($_.date) -or ($_.date -ge $dateStart -and $_.date -le $dateEnd)} | sort date | Format-Table -autosize
 }
 $nearbymarriages = $allData | ?{$_.marrdate -and $_.marrplac -match $placeFilter} | %{
     $marRec = $_; 
@@ -37,4 +39,4 @@ $nearbymarriages = $allData | ?{$_.marrdate -and $_.marrplac -match $placeFilter
     $wifey = $allData | ?{$_.given -and $_.num -eq $marRec.wife_num}; 
     new-object psobject -Property @{placeprop='marrplac';date=$(sanitizeDate($marRec.marrdate)); place=$marrec.marrplac;hubby_name=$($hubby.given + " " + $hubby.surname); hubby_fid=$_.husb_fid; wifey_name=$($wifey.given + " " + $wifey.surname); wifey_fid=$_.wife_fid}}
 
-$nearbymarriages | select placeprop, date, place, hubby_name, wifey_name, hubby_fid, wifey_fid | sort date | Format-Table -autosize
+$nearbymarriages | select placeprop, date, place, hubby_name, wifey_name, hubby_fid, wifey_fid | ?{!($_.date) -or ($_.date -ge $dateStart -and $_.date -le $dateEnd)} | sort date | Format-Table -autosize
